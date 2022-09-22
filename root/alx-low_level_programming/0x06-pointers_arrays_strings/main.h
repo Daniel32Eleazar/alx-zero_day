23788a076c677dbf58788e9430166380f30aa79a
Daniel <danieleleazar65@gmail.com>
2022-09-21 17:50:52 -0700

    my  commit

---
+++ root/alx-low_level_programming/0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,35 @@
+ar *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+int _putchar(char);
+cat main.h
+echo 'main' > README.md
+cd ..
+echo 'main' > README.md
+cd 0x06-pointers_arrays_strings
+chmod u+x main.h
+git add .
+git commit -m "my  commit"
+git push
+
